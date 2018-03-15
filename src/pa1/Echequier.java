4442580a92b17cec8722cc8316e789f8f1010df8
duquennl <duquennoy.loic@gmail.com>
2018-03-15 15:30:32 +0100

    initialisation

---
+++ src/pa1/Echequier.java
@@ -0,0 +1,15 @@
+package pa1;
+
+public class Echequier {
+
+	private char [][]echequier;
+	private int []coordonnee;
+	public Echequier(char [][]echequier)
+	{
+	}
+	public char deplacement(int []coordonnee)
+	{
+		
+		
+	}
+}
---
+++ src/pa1/Piece.java
@@ -0,0 +1,8 @@
+package pa1;
+
+abstract class Piece {
+	 abstract void deplacer(echiquier)
+	 {
+	 }
+	 }
+}
