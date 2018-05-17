41a9bcb8a6ebf4c6b183a19129bcadc5cfa2a1b7
Plinz <antoine.duquennoy@protonmail.com>
2018-05-17 13:50:00 +0200

    structure de jeu

--- src/pa1/Main.java
+++ src/pa1/Main.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-
-		Echequier echec = new Echequier();
-		System.out.println("Veuillez saisir un mot :");
-		Scanner sc = new Scanner(System.in);
-		int str = sc.nextInt();
-		System.out.println("Vous avez saisi : " + str);
+		Partie p = new Partie();
+		p.init();
+		while(p.estFini()){
+			p.jouer();
+		}
+//		Echequier echec = new Echequier();
+//		System.out.println("Veuillez saisir un mot :");
+//		Scanner sc = new Scanner(System.in);
+//		int str = sc.nextInt();
+//		System.out.println("Vous avez saisi : " + str);
 	}
 }
---
+++ src/pa1/Partie.java
@@ -0,0 +1,46 @@
+package pa1;
+
+public class Partie {
+	Echequier plateau;
+
+	/**
+	 * Methode d'initialisation du jeu :
+	 * 	- Choix des noms de joueurs ?
+	 *  - Choix des couleurs
+	 *  - Methode de jeu (avec/sans temps limite par exemple)
+	 *  - ...
+	 */
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Methode de detection de fin de partie
+	 * @return Vrai si il y a un echec et mat ou si il y a un abandon
+	 */
+	public boolean estFini() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 * Methode principale ou le jeu se passe
+	 */
+	public void jouer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Methode de fin de jeu :
+	 *  - Annonce des gagnants
+	 *  - Possibilité de stats
+	 *  - Possibilité de regarder les coups de la partie
+	 */
+	public void finDeJeu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
